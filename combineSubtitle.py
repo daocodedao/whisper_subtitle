8b1314dd5fff7c253180e5552e8faac7ab54d9ad
linzhiji <linzhiji@gmail.com>
2024-02-20 14:30:26 +0800

    字幕矫正

--- combineSubtitle.py
+++ combineSubtitle.py
@@ -6,7 +6,9 @@ def combinSubtitle(outPutMp4, subtitleDstPath, outPutSubtitleMp4):
     # ffcommand=f"ffmpeg -i {outPutMp4} -vf subtitles={subtitleDstPath}:force_style='FontName=simsun' {outPutSubtitleMp4}"
     if os.path.isfile(outPutSubtitleMp4):
         os.remove(outPutSubtitleMp4)
-    ffcommand=f"ffmpeg -y -i {outPutMp4} -vf subtitles=\"{subtitleDstPath}:force_style='FontName=ubuntu,Alignment=2,BorderStyle=3,Outline=1,Shadow=0,FontSize=8'\" {outPutSubtitleMp4}"
+    # ffmpeg -i source_video_path.mp4 -vf "subtitles=srt_source.srt:force_style='OutlineColour=&H80000000,BorderStyle=4,BackColour=&H80000000,Outline=0,Shadow=0,MarginV=25,Fontname=Arial,Fontsize=10,Alignment=2'" video_destination.mp4
+# 
+    ffcommand=f"ffmpeg -y -i {outPutMp4} -vf subtitles=\"{subtitleDstPath}:force_style='FontName=ubuntu,FontSize=8,Alignment=2,OutlineColour=&H80000000,BorderStyle=4,BackColour=&H80000000,Outline=0,Shadow=0,MarginV=25'\" {outPutSubtitleMp4}"
     api_logger.info(f"FFMPEG 合成字幕命令：")
     api_logger.info(ffcommand)
     result = subprocess.check_output(ffcommand, shell=True)
