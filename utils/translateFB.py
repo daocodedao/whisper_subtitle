6a37188043ffb49c2e080141abd1d7e621165055
linzhiji <linzhiji@gmail.com>
2024-02-23 21:31:57 +0800

    fix bug

--- utils/translateFB.py
+++ utils/translateFB.py
@@ -1,14 +1,19 @@
 
 import requests
+import socket
 
 
 def translate_en_to_zh(srcText):
     
     # 外网
     # url = "http://39.105.194.16:9890/translate/en-cn/"
+    url = "http://192.168.3691.:9890/translate/en-cn/"
     
-    # s 机房 /data/work/aishowos/bitwit-bot/start-translate-server.sh
-    url = "http://127.0.0.1:9890/translate/en-cn/"
+    localIp = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1]
+    if "192.168.0.69" in localIp:
+        # s 机房 /data/work/aishowos/bitwit-bot/start-translate-server.sh
+        url = "http://127.0.0.1:9890/translate/en-cn/"
+
 
     data = {
         "srcLang": "en_XX",
