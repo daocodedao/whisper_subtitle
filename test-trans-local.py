fed4414a1b015fc9a05575b318428fd45582102d
linzhiji <linzhiji@gmail.com>
2024-02-23 18:37:14 +0800

    fix bug

--- test-trans-local.py
+++ test-trans-local.py
@@ -4,11 +4,12 @@ import os
 os.environ['HTTP_PROXY'] = '192.168.0.77:18808'
 os.environ['HTTPS_PROXY'] = '192.168.0.77:18808'
 
-article_cn = "This model is a fine-tuned checkpoint of mBART-large-50. mbart-large-50-many-to-many-mmt is fine-tuned for multilingual machine translation. "
 
 model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
 tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
 
+
+article_cn = "This model is a fine-tuned checkpoint of mBART-large-50. mbart-large-50-many-to-many-mmt is fine-tuned for multilingual machine translation. "
 
 tokenizer.src_lang = "en_XX"
 encoded_zh = tokenizer(article_cn, return_tensors="pt")
