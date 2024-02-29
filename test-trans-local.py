6f61cf34f03e7d815d5a2147e17b670964e11712
linzhiji <linzhiji@gmail.com>
2024-02-29 19:01:37 +0800

    Initial commit

--- test-trans-local.py
+++ test-trans-local.py
@@ -3,31 +3,9 @@ import requests
 import time
 import json
 
-appKey = "5A77CE4169854865B7D3714BD39A78C6"
-param = {
-    "textList": ["Trump was always bothered by how Trump Tower fell 41 feet short of the General Motors building two blocks north."],
-    "appId": "FFFFB26AF7A64AB59620240229114219",
-    "domain": "default",
-    "from": "en",
-    "to": "zh"
-}
-ts = str(int(time.time() * 1000))
+from utils.translateBaidu import *
 
-concatString = appKey + json.dumps(param) + ts
-secretKey = hashlib.md5(concatString.encode()).hexdigest()
 
-url = "https://translate.10jqka.com.cn/translateApi/batch/v2/get/result"
-body = {
-    "param": json.dumps(param),
-    "secretKey": secretKey,
-    "ts": ts
-}
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded"
-}
+retStr = replaceSpecialWordEnToZh("这笔交易很复杂。Conseco投资2.11亿美元，Trump出资1100万美元，")
 
-response = requests.post(url, data=body, headers=headers)
-if response.status_code == 200:
-    print(response.text)
-else:
-    print("Request failed with response code:", response.status_code)
+print(retStr)
--- utils/translateBaidu.py
+++ utils/translateBaidu.py
@@ -1,7 +1,22 @@
 
 import requests
 import socket
-from tans_base import *
+import json
+
+
+
+def replaceSpecialWordEnToZh(srcStr:str = None):
+    if srcStr is None:
+        return ""
+    # 读取JSON文件
+    with open("./utils/trans-en-zh-dic.json", "r") as file:
+        wordList = json.load(file)
+        for word in wordList:
+            if word["en"] in srcStr:
+                srcStr = srcStr.replace(word["en"], word["zh-cn"])
+
+    return srcStr
+
 
 
 
