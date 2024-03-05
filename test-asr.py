dc9d7cbcd31aa24977c41b6edbbe9770bda26844
linzhiji <linzhiji@gmail.com>
2024-03-05 16:29:41 +0800

    asr

--- test-asr.py
+++ test-asr.py
@@ -1,15 +1,18 @@
 from funasr import AutoModel
 # paraformer-zh is a multi-functional asr model
 # use vad, punc, spk or not as you need
-model = AutoModel(model="paraformer-zh",  vad_model="fsmn-vad",  punc_model="ct-punc-c", 
-                  # spk_model="cam++", 
-                  )
+# model = AutoModel(model="paraformer-zh",  vad_model="fsmn-vad",  punc_model="ct-punc-c", 
+#                   # spk_model="cam++", 
+#                   )
+model = AutoModel(model="fa-zh")
+
 audioPath="./sample/simple5-combine.mp3"
 text_file="./out/simple5-combine.txt"
 # res = model.generate(input=audioPath, 
 #                      batch_size_s=300, 
 #                      hotword='魔搭')
 
-res = model.generate(input=(audioPath, text_file), data_type=("sound", "text"))
+res = model.generate(input=(audioPath, text_file), 
+                     data_type=("sound", "text"))
 
 print(res)
