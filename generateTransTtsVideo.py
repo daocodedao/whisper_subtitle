d82ed7d4967dfe165252a60b363f0ca8f2471ca3
linzhiji <linzhiji@gmail.com>
2024-03-07 18:40:11 +0800

    fix bug

--- generateTransTtsVideo.py
+++ generateTransTtsVideo.py
@@ -230,7 +230,7 @@ def translate_srt(outSrtCnPath, inSrtFilePath, isVerticle = True):
             if len(curLineEnContent) > 0:
                 lastChar = curLineEnContent[len(curLineEnContent) - 1]
                 # 结尾不是标点符号, 准备操作
-                if lastChar not in enPunctuations and index + 1 <= len(enAllSubList): 
+                if lastChar not in enPunctuations and index + 1 < len(enAllSubList): 
                     api_logger.info(f"当前是{index}行。末尾不是.?, 继续取下一行。")
                     continue
 
