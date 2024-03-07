2ac243147a738fc2c90910e0a89d45eebf71a9a5
linzhiji <linzhiji@gmail.com>
2024-03-07 15:18:43 +0800

    fix bug

--- generateTransTtsVideo.py
+++ generateTransTtsVideo.py
@@ -505,7 +505,7 @@ if check_video_verticle(videoPath):
 api_logger.info("1---------视频生成英文SRT")
 
 api_logger.info(f"从视频剥离音频文件 {srcAudioPath}")
-command = f"ffmpeg -i {videoPath} -vn -acodec pcm_f32le -ar 44100 -ac 2 {srcAudioPath}"
+command = f"ffmpeg -y -i {videoPath} -vn -acodec pcm_f32le -ar 44100 -ac 2 {srcAudioPath}"
 api_logger.info(command)
 # command = f"ffmpeg -y -i {videoPath} -vn -acodec copy {srcAudioPath}"
 result = subprocess.check_output(command, shell=True)
