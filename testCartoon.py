03b332fd3f9ea8d5fbd01dab367d0db4663ca7a2
linzhiji <linzhiji@gmail.com>
2024-03-15 00:59:29 +0800

    fix bug

--- testCartoon.py
+++ testCartoon.py
@@ -24,7 +24,8 @@ pipeline = StableDiffusionInstructPix2PixPipeline.from_pretrained(
 # torch.backends.cudnn.benchmark = True
 # pipeline.enable_xformers_memory_efficient_attention()
 # pipeline.enable_model_cpu_offload()
-image_path = "/data/work/translate/BiB9YykxoZw/frames/11.png"
+fileIdx = 37
+image_path = f"/data/work/translate/BiB9YykxoZw/frames/{fileIdx}.png"
 image = load_image(image_path)
 num_inference_steps = 20
 image_guidance_scale = 1.5
@@ -35,7 +36,7 @@ image = pipeline("Cartoonize the following image",
                     image_guidance_scale=image_guidance_scale,
                     guidance_scale=guidance_scale
                     ).images[0]
-cartoonImagePath = "/data/work/translate/BiB9YykxoZw/cartoon/11.png"
+cartoonImagePath = f"/data/work/translate/BiB9YykxoZw/cartoon/{fileIdx}.png"
 image.save(cartoonImagePath)
 
 
