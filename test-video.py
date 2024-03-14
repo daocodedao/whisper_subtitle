6076736607933c6dd820f4ff544bf4e55d4e19f3
linzhiji <linzhiji@gmail.com>
2024-03-14 19:37:10 +0800

    fix bug

--- test-video.py
+++ test-video.py
@@ -1,7 +1,13 @@
 
 from moviepy.editor import *
 from utils.util import Util
+import re
 
+def sort_human(l):
+    convert = lambda text: float(text) if text.isdigit() else text
+    alphanum = lambda key: [convert(c) for c in re.split('([-+]?[0-9]*\.?[0-9]*)', key)]
+    l.sort(key=alphanum)
+    return l
 # videoSrcPath = "./sample/simple5.mp4"
 # clip = VideoFileClip(videoSrcPath)
 # print("done")
@@ -11,7 +17,8 @@ outVideoPath="./out/simple5/simple5-cartoon.mp4"
 kFixedFps=24
 result_frames = Util.get_image_paths_from_folder(frameOutDir)
 
-result_frames.sort()
+# result_frames.sort()
+result_frames = sort_human(result_frames)
 print(result_frames)
 final_vid = Util.create_video(result_frames, kFixedFps, outVideoPath)
 # api_logger.info(f"视频保存到 {outVideoPath}")
