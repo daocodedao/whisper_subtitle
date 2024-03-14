159ba5178b09a41b1052f5107dd32cba0ec9e258
linzhiji <linzhiji@gmail.com>
2024-03-15 01:03:15 +0800

    fix bug

--- testCartoon.py
+++ testCartoon.py
@@ -30,6 +30,8 @@ image = load_image(image_path)
 num_inference_steps = 20
 image_guidance_scale = 1.5
 guidance_scale = 7.5 
+
+
 image = pipeline("Cartoonize the following image", 
                     image=image,
                     num_inference_steps=num_inference_steps,
@@ -39,6 +41,25 @@ image = pipeline("Cartoonize the following image",
 cartoonImagePath = f"/data/work/translate/BiB9YykxoZw/cartoon/{fileIdx}.png"
 image.save(cartoonImagePath)
 
+k10K = 10 * 1024
+kMinFileSizeK = k10K
+
+for tryIdx in range(100):
+    fileSize = os.path.getsize(cartoonImagePath) 
+    if fileSize < kMinFileSizeK:
+        api_logger.error(f"文件 {fileSize} < {kMinFileSizeK} 生成错误, 重试{tryIdx}次")
+        image = pipeline("Cartoonize the following image", 
+                    image=image,
+                    num_inference_steps=num_inference_steps,
+                    image_guidance_scale=image_guidance_scale,
+                    guidance_scale=guidance_scale
+                    ).images[0]
+        cartoonImagePath = f"/data/work/translate/BiB9YykxoZw/cartoon/{fileIdx}.png"
+        image.save(cartoonImagePath)
+    else:
+        break
+
+api_logger.info(f"完成")
 
 # num_inference_steps 默认100
 # image_guidance_scale 默认 1.5 , 接近原图的参数，越高越接近，最少1
