36fed9fc18db0bf7f442f0d5bc4897eb2dcd5568
linzhiji <linzhiji@gmail.com>
2024-03-15 10:47:37 +0800

    change model

--- test-cartoon.py
+++ test-cartoon.py
@@ -9,6 +9,7 @@ import argparse
 import moviepy.editor as mp
 import PIL
 import time
+import random
 
 os.environ['HTTP_PROXY'] = '192.168.0.77:18808'
 os.environ['HTTPS_PROXY'] = '192.168.0.77:18808'
@@ -17,6 +18,7 @@ api_logger.info("准备开始")
 
 api_logger.info("---------加载模型")
 model_id = "timbrooks/instruct-pix2pix"
+# model_id = "instruction-tuning-sd/cartoonizer"
 pipeline = StableDiffusionInstructPix2PixPipeline.from_pretrained(
     model_id, torch_dtype=torch.bfloat16, use_auth_token=True
 ).to("cuda")
@@ -27,8 +29,6 @@ pipeline = StableDiffusionInstructPix2PixPipeline.from_pretrained(
 fileIdx = 37
 image_path = f"/data/work/translate/BiB9YykxoZw/frames/{fileIdx}.png"
 image = load_image(image_path)
-image = PIL.ImageOps.exif_transpose(image)
-image = image.convert("RGB")
 
 num_inference_steps = 20
 image_guidance_scale = 1
@@ -52,9 +52,9 @@ for tryIdx in range(100):
     fileSize = os.path.getsize(cartoonImagePath) 
     if fileSize < kMinFileSizeK:
         api_logger.error(f"文件 {fileSize} < {kMinFileSizeK} 生成错误, 重试{tryIdx}次")
-        num_inference_steps = 20
+        num_inference_steps = random.randint(2, 100)
         image_guidance_scale = 1.5
-        guidance_scale = 7.5 
+        guidance_scale = 7 
 
         image = pipeline("Cartoonize the following image", 
                     image=image,
