69c916bc0e4c1efea580675a29a27977203e33a0
linzhiji <linzhiji@gmail.com>
2024-04-09 16:20:58 +0800

    fix bug

--- test/test-asr.py
+++ test/test-asr.py
@@ -3,7 +3,7 @@ import srt,sys,os
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import utilAsr
 
-videoDir = "/Users/linzhiji/Downloads/longvideo"
+videoDir = "/data/work/translate/8BxjAbLWfrM"
 processId = "8BxjAbLWfrM"
 combineMp3Path = f"{videoDir}/{processId}/{processId}.mp3"
 outSrtAsrCnPath = os.path.join(videoDir, f"{processId}-asr-cn.srt")
