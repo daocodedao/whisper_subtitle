5444e98a34873815a1d698dbc1c3d0ba1b000ef0
linzhiji <linzhiji@ishehui.com>
2024-09-23 16:02:33 +0800

    fix  bug

--- test/test-audio-silience.py
+++ test/test-audio-silience.py
@@ -7,4 +7,7 @@ from utils.logger_settings import api_logger
 
 silence_duration=6000.0
 api_logger.info(f"需要加入静音音频， 时长：{silence_duration}毫秒")
-second_of_silence = AudioSegment.silent(duration=silence_duration)
+try:
+    second_of_silence = AudioSegment.silent(duration=silence_duration)
+except Exception as e:
+    api_logger.error(f"原视频静音失败：{e}")
